6b5663df783447e955566ff16a96bec6867651e4
kibetamos <amos.rotich@strathmore.edu>
2025-11-18 10:18:50 +0300

    added Workflows

--- EmployeeApp/models.py
+++ EmployeeApp/models.py
@@ -1,18 +1,27 @@
+
 from django.db import models
 
+
 class Department(models.Model):
-    DepartmentId = models.AutoField(primary_key=True)
-    DepartmentName = models.CharField(max_length=500)
+    department_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500, unique=True)
 
     def __str__(self):
-        return self.DepartmentName
+        return self.name
+
 
 class Employee(models.Model):
-    EmployeeId = models.AutoField(primary_key=True)
-    EmployeeName = models.CharField(max_length=500)
-    Department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
-    DateOfJoining = models.DateField()
-    PhotoFileName = models.CharField(max_length=500)
+    employee_id = models.AutoField(primary_key=True)
+    id_number = models.CharField(max_length=100, unique=True, blank=True)
+    name = models.CharField(max_length=500)
+    department = models.ForeignKey(
+        Department,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="employees"
+    )
+    date_of_joining = models.DateField()
+    photo_file_name = models.CharField(max_length=500, blank=True)
 
     def __str__(self):
-        return self.EmployeeName
+        return self.name
